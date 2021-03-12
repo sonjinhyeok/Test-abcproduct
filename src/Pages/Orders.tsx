eaf3bfc8b57deaf2901792ea1efc7e89c9db563d
sonjinhyeok <jinhyeok.son@axas-japan.co.jp>
2021-03-12 11:42:43 +0900

    Buttons SCSS

--- src/Pages/Orders.tsx
+++ src/Pages/Orders.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../components/Header/Header';
 
 const Order = () => {
     return(
         <div className="container">
+            <Header title="作成中。。。"/>
         </div>
     );
 }
