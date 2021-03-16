7bfb58202dfb7b27b16afe9c984eea0834aee348
sonjinhyeok <jinhyeok.son@axas-japan.co.jp>
2021-03-16 18:30:55 +0900

    config file settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,23 @@
 module.exports = {
-    mode: "development",
-  
-    entry: "./src/scssTest/main.ts",
-  
-    module: {
+    mode: "development",                 // development に設定するとソースマップ有効でJSファイルが出力される
+    entry: "./src/index.ts",             // メインとなるファイル（エントリーポイント）
+    
+    output: {                            // ファイルの出力設定
+      path: `${__dirname}/dist`,         //  出力ファイルのディレクトリ名
+      filename: "main.ts"                // 出力ファイル名
+    },
+    module: {                            // ファイルの出力設定
       rules: [
         {
-          test: /\.ts$/,
-          use: "ts-loader",
+          test: /\.ts$/,                 // 拡張子 .ts の場合
+          use: "ts-loader",              // TypeScript をコンパイルする
         },
-        // scssファイルに対する処理
         {
-          test: /\.scss$/,
+          test: /\.scss$/,               // scssファイルに対する処理
           use: [
-            { loader: "style-loader" },
+            { 
+              loader: "style-loader" 
+            },
             {
               loader: "css-loader",
               options: {
@@ -34,7 +38,8 @@ module.exports = {
         },
       ],
     },
-  
+    
+    // import 文で .ts ファイルを解決するため
     resolve: {
       extensions: [".js", ".ts"],
     },
